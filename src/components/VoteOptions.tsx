cadd91b373080e228e6992fb6be9195520d1030c
Hyria Roman <hyrya.roman@gmail.com>
2026-06-11 16:12:49 +0300

    app: fix VoteOptions props

--- src/components/VoteOptions.tsx
+++ src/components/VoteOptions.tsx
@@ -4,7 +4,7 @@ import css from "./VoteOptions.module.css";
 interface VoteOptionsProps {
   onVote: (vote: VoteType) => void;
   onReset: () => void;
-  canReset: bool;
+  canReset: boolean;
 }
 
 function VoteOptions(props: VoteOptionsProps) {

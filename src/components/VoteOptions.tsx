3581b3aee2e7eefd724d4ab8bde33ce852a69ea2
Hyria Roman <hyrya.roman@gmail.com>
2026-06-11 16:07:11 +0300

    app: hide reset button when no votes are available

--- src/components/VoteOptions.tsx
+++ src/components/VoteOptions.tsx
@@ -10,34 +10,23 @@ interface VoteOptionsProps {
 function VoteOptions(props: VoteOptionsProps) {
   return (
     <div className={css.container}>
-      <button
-        className={css.button}
-        onClick={() => props.onVote("good")}
-        disabled={!props.canReset}
-      >
+      <button className={css.button} onClick={() => props.onVote("good")}>
         Good
       </button>
-      <button
-        className={css.button}
-        onClick={() => props.onVote("neutral")}
-        disabled={!props.canReset}
-      >
+      <button className={css.button} onClick={() => props.onVote("neutral")}>
         Neutral
       </button>
-      <button
-        className={css.button}
-        onClick={() => props.onVote("bad")}
-        disabled={!props.canReset}
-      >
+      <button className={css.button} onClick={() => props.onVote("bad")}>
         Bad
       </button>
-      <button
-        className={`${css.button} ${css.reset}`}
-        onClick={() => props.onReset()}
-        disabled={!props.canReset}
-      >
-        Reset
-      </button>
+      {props.canReset && (
+        <button
+          className={`${css.button} ${css.reset}`}
+          onClick={() => props.onReset()}
+        >
+          Reset
+        </button>
+      )}
     </div>
   );
 }

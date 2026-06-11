bc3ff7d42c032387780a8598ee4fe28811b899ae
Hyria Roman <hyrya.roman@gmail.com>
2026-06-11 15:24:58 +0300

    app: add vote count state

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,35 @@
+import { useState } from "react";
+
+import type { Votes, VoteType } from "../types/votes.ts";
+import CafeInfo from "./CafeInfo.tsx";
+import css from "./App.module.css";
+
 function App() {
+  const [votes, setVotes] = useState<Votes>({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  function handleVote(type: VoteType) {
+    setVotes({
+      ...votes,
+      [type]: votes[type] + 1,
+    });
+  }
+
+  function resetVotes() {
+    setVotes({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  }
+
   return (
-    <>
-      <h1>Hello, world!</h1>
-    </>
+    <div className={css.app}>
+      <CafeInfo />
+    </div>
   );
 }
 
